13838276e8c6f6c5339b63559e928593c7d4d853
dima.chernyshev.1976 <dima.chernyshev.1976@mail.ru>
2019-06-10 23:52:40 +0300

    Добавьте файлы проекта.

---
+++ ExpressApp1/app.js
@@ -0,0 +1,19 @@
+﻿const express = require("express");
+
+const app = express();
+
+app.set("view engine", "hbs");
+
+app.use("/contact", function (request, response) {
+
+    response.render("contact.hbs", {
+        title: "Мои контакты",
+        email: "gavgav@mycorp.com",
+        phone: "+1234567890"
+    });
+});
+app.use("/", function (request, response) {
+
+    response.send("Главная страница");
+});
+app.listen(3000);
